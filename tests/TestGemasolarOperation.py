27d3c6996a4972b8ba21d3c05157653179ed9e23
arfontalvo <armando.fontalvo@anu.edu.au>
2022-09-14 19:42:47 +1000

    Parse file names into GemasolarOperation

--- tests/TestGemasolarOperation.py
+++ tests/TestGemasolarOperation.py
@@ -20,8 +20,36 @@ class TestFlowPathStress(unittest.TestCase):
 		sim.load_init()
 		T = 565 + 273.15
 		h = 1396.0182*T + 0.086*pow(T,2)
-		sim.update_pars(['T_hot_set','T_hot_start','state_hot_set.h','controlCold.state_ref.h'],
-			[str(T),str(T),str(h),str(h)])
+		T = str(T)
+		h = str(h)
+		parent = os.path.join(os.path.expanduser('~'),'ownCloud/phd_update/damage/N06230/N06230_OD22.40_WT1.20_565')
+		par_n = [
+			'T_hot_set'
+			,'T_hot_start'
+			,'state_hot_set.h'
+			,'controlCold.state_ref.h'
+			,'opt_file'
+			,'hav_file'
+			,'file_dni1'
+			,'file_dni2'
+			,'file_dni3'
+			,'file_dni4'
+			,'file_mflow'
+		]
+		par_v = [
+			T
+			,T
+			,h
+			,h
+			,os.path.join(parent,'OELTs_Solstice.motab')
+			,os.path.join(parent,'HALTs_Solstice.motab')
+			,os.path.join(parent,'FLUX_fp1_d0.56.motab')
+			,os.path.join(parent,'FLUX_fp1_d0.87.motab')
+			,os.path.join(parent,'FLUX_fp1_d1.0.motab')
+			,os.path.join(parent,'FLUX_fp1_d1.39.motab')
+			,os.path.join(parent,'MFLOW_Solstice_fp1.motab')
+		]
+		sim.update_pars(par_n,par_v)
 		sim.simulate(start=0, stop='1y', step='5m', solver='dassl', nls='homotopy')
 		self.res = postproc.SimResultElec(sim.res_fn)
 		self.perf = self.res.calc_perf()
